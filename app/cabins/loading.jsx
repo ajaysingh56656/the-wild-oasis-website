4b2efd27d5b74a1845b8f4d33b4b475fec108b6e
ajaysingh56656 <ajaysingh56656@gmail.com>
2024-10-16 14:30:19 +0530

    streaming route segments with loading.js file

---
+++ app/cabins/loading.jsx
@@ -0,0 +1,13 @@
+import Spinner from "../_components/Spinner";
+
+
+const Loading = () => {
+  return (
+    <div className="grid items-center justify-center">
+      <Spinner />
+      <p className="text-xl text-primary-200">Loading Cabin Data</p>
+    </div>
+  );
+};
+
+export default Loading;
