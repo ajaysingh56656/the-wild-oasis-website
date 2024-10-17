42a8d2c81ffcc839381da4d083243cef865e705c
ajaysingh56656 <ajaysingh56656@gmail.com>
2024-10-17 12:13:25 +0530

    Caching and ISR

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,10 @@ import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
 
+// export const revalidate = 0;
+// export const revalidate = 15;
+// export const revalidate = 3600;
+
 export const metadata = {
   title: "Cabins",
   description: "This is cabins page",
@@ -17,9 +21,9 @@ function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
 
       <Suspense fallback={<Spinner />}>
